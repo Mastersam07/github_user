0cb12663e1548bbc3040314837eea2083a0847aa
Mastersam <abadasamuelosp@gmail.com>
2021-02-22 13:31:03 +0100

    fixed filename issue

--- lib/features/user_fetch/presentation/view_models/fetch_user_repos_View_model.dart
+++ lib/features/user_fetch/presentation/view_models/fetch_user_repos_View_model.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+
 import '../../../../core/core_presentation/core_view_model/core_view_model.dart';
 import '../../data/models/githup_repo_model.dart';
 import '../../domain/use_cases/fetch_user_repo.dart';
-import 'package:meta/meta.dart';
 
 class FetchReposViewModel extends BaseViewModel {
   final FetchUserGithubRepositories fetchUserGithubRepositories;
