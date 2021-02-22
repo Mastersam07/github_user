513442e795cb94ef6c50341a24896fee0c1c560c
Mastersam <abadasamuelosp@gmail.com>
2021-02-22 13:35:47 +0100

    lint fix

---
+++ lib/features/user_fetch/presentation/view_models/fetch_user_repo_view_model.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+
+import '../../../../core/core_presentation/core_view_model/core_view_model.dart';
+import '../../data/models/githup_repo_model.dart';
+import '../../domain/use_cases/fetch_user_repo.dart';
+
+class FetchReposViewModel extends BaseViewModel {
+  final FetchUserGithubRepositories fetchUserGithubRepositories;
+
+  FetchReposViewModel({@required this.fetchUserGithubRepositories});
+
+  List<GithubRepo> userRepo = [];
+  Future<void> fetchUserDetails(BuildContext context, String login) async {
+    setState(viewState: ViewState.loading);
+    final result = await fetchUserGithubRepositories(login);
+    result.fold((failure) => handleFailure(context, failure), (fetchedRepo) {
+      userRepo = fetchedRepo;
+      print(fetchedRepo[0].name);
+    });
+    setState(viewState: ViewState.idle);
+  }
+}
